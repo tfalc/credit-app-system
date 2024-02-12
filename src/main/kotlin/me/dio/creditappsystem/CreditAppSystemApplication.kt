546b9250ab741d3003570a0d615d512681c7fdfd
Thiago F <thiago.falcao86@gmail.com>
2024-02-11 22:19:59 -0300

    Initial commit

---
+++ src/main/kotlin/me/dio/creditappsystem/CreditAppSystemApplication.kt
@@ -0,0 +1,11 @@
+package me.dio.creditappsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditAppSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditAppSystemApplication>(*args)
+}
